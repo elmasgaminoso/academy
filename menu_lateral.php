65d53579cf3814d744d2c55ddf5d443bf3323663
elmasgaminoso <juandap00@hotmail.com>
2022-05-18 16:31:57 -0500

    se creo el menu lateral

--- menu_lateral.php
+++ menu_lateral.php
@@ -1,4 +1,5 @@
 
+<a data-target="slide-out" class="sidenav-trigger"href="#">
 <ul id="nav-mobile" class="sidenav sidenav-fixed menu_lateral" style="transform: translateX(0px);">
         <li><svg xmlns="http://www.w3.org/2000/svg" width="80" height="69" viewBox="0 0 80 69" fill="none"><path d="M38.2421 68.1732H38.2166C17.6597 68.3786 0.767037 52.1534 0.0242494 31.7319C0.0114867 31.3937 0.0038288 31.0555 0 30.7147H0.025525C6.83314 29.2049 3.49187 51.0431 21.1222 52.6678C21.3481 52.6882 21.5715 52.7112 21.7936 52.7341C20.8083 51.6059 19.7656 50.4841 18.7994 49.583C10.9925 42.2955 27.9515 47.4619 35.3194 58.2897C37.0539 60.4403 38.0621 63.3642 38.2217 67.341C38.2319 67.6142 38.2395 67.8911 38.2421 68.1732Z" fill="url(#paint0_linear_203_15)"></path><path d="M41.5936 68.1732H41.6191C62.176 68.3786 79.0687 52.1534 79.8115 31.7319C79.8242 31.3937 79.8319 31.0555 79.8357 30.7147H79.8102C73.0026 29.2049 76.3438 51.0431 58.7135 52.6678C58.4876 52.6882 58.2642 52.7112 58.0422 52.7341C59.0274 51.6059 60.0702 50.4841 61.0363 49.583C68.8432 42.2955 51.8842 47.4619 44.5163 58.2897C42.7818 60.4403 41.7736 63.3642 41.614 67.341C41.6038 67.6142 41.5962 67.8911 41.5936 68.1732Z" fill="url(#paint1_linear_203_15)"></path><path d="M21.9799 28.799L21.5562 30.0293V28.6496L21.9799 28.799Z" fill="#019147"></path><path d="M58.9878 28.6714V29.9859L58.5845 28.799L58.9878 28.6714Z" fill="#019147"></path><path d="M40.2765 55.9146C40.2765 55.9146 40.7997 39.4788 62.4708 33.6475L61.2775 30.1352L66.454 28.4314L69.429 37.4253C69.429 37.4253 50.091 39.2797 40.2752 55.9146H40.2765ZM40.2765 55.9146C40.2765 55.9146 39.7532 39.4788 18.0822 33.6475L19.2755 30.1352L14.0989 28.4314L11.124 37.4253C11.124 37.4253 30.462 39.2797 40.2777 55.9146H40.2765Z" fill="#EEC45B"></path><path d="M68.5292 11.452L52.2236 18.0605L48.4076 19.606L40.2765 22.8988L32.0918 19.5831L28.2694 18.0349L12.0237 11.452L40.2765 0L68.5292 11.452Z" fill="#264984"></path><path d="M58.9878 18.9156V32.5104C47.8958 36.2434 42.7001 42.7958 40.2828 47.9915C37.8439 42.7741 32.6482 36.2001 21.4719 32.467C21.4719 31.8314 21.4936 31.2366 21.5574 30.6215V18.9143L25.7142 20.59L40.2841 26.4851L53.4756 21.1414L54.7698 20.6321L58.9904 18.9143L58.9878 18.9156Z" fill="#264984"></path><defs><linearGradient id="paint0_linear_203_15" x1="0" y1="49.4069" x2="38.2421" y2="49.4069" gradientUnits="userSpaceOnUse"><stop stop-color="#34BC87"></stop><stop offset="1" stop-color="#40BEDD"></stop></linearGradient><linearGradient id="paint1_linear_203_15" x1="41.5936" y1="49.4069" x2="79.8344" y2="49.4069" gradientUnits="userSpaceOnUse"><stop stop-color="#40BEDD"></stop><stop offset="1" stop-color="#34BC87"></stop></linearGradient></defs></svg></li>
         <li class="version"><a href="#" data-target="version-dropdown" class="dropdown-trigger">
@@ -13,6 +14,7 @@
         <li class="bold"><a href="getting-started.html" class="waves-effect waves-teal">Getting Started</a></li>
       
 </ul>
+</a>
 <ul id="slide-out" class="sidenav">
     <li><div class="user-view">
       <div class="background">
@@ -33,6 +35,7 @@
   
 
 <script>
+      M.AutoInit();
     document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
