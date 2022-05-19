ae4da96720d58b1d34947f56b8e6bfa62eede39d
elmasgaminoso <juandap00@hotmail.com>
2022-05-18 19:01:14 -0500

    :V

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 $nombre_usuario= $row["nombre"] ." ". $row["apellido"];
 ?>
 <nav>
-  <div class="nav-wrapper">
+  <div class="nav-wrapper container">
     <a href="" class="brand-logo">Logo</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li><a href="">Hola, <?php echo $nombre_usuario ?></a></li>
