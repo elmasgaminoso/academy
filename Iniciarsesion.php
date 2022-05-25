2c36812b8497955af12d56f3cb41bb4930fb9007
elmasgaminoso <juandap00@hotmail.com>
2022-05-25 13:05:40 -0500

    Ajuste inicio de session

--- Iniciarsesion.php
+++ Iniciarsesion.php
@@ -12,26 +12,31 @@
   </head>
   <body class="fondo_inicio">
   <div class="container center white z-depth-3 sesion_contenedor">
-    <div class="row">
-      <div class="col l6">
+    <div class="row cajainicio">
+      <div class="col l6 formulario">
 	   <form  method="post" action="">
       <div class="row">
         <h2>Iniciar sesión</h2>
         <p>Digite su correo electrónico y contraseña para ingresar al Dashboard. </p>
-        <div class="input-field">
+        <p class="black-text campos">Usuario:</p>
+        <div class="input-field field">
            <i class="material-icons prefix">account_circle</i>
           <input placeholder="Usuario" id="first_name" type="text" class="validate black-text" name="usuario">
-          <label for="first_name" class="black-text">Usuario:</label>
         </div>
-		  <div class="input-field ">
-           <i class="material-icons prefix">vpn_key</i>
+        <p class="black-text campos">Contraseña:</p>
+		  <div class="input-field field">
+           <i class="material-icons prefix">lock</i>
           <input placeholder=" Contraseña " id="first_name" type="password" class="validate black-text" name="clave">
-          <label for="first_name" class="black-text">Contraseña:</label>
         </div>
-		  <div class=" center">
-        <a class="waves-effect waves-light btn-large shake-slow grey"><i class="material-icons left  ">send</i><input type="submit" value="¡Iniciar sesión!" name="buscar"></input></a> 
+		  <div class=" right">
+        <a class="waves-effect waves-light btn-large shake-slow btninicio"><input type="submit" value="¡Iniciar sesión!" name="buscar"></input></a> 
       </div>
       </div>
+    </div>
+    <div class="col l6 logocaja">
+      <img class="responsive-img" src="img/Logo.png">
+      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_fq7pwzey.json"  background="transparent"  speed="1"  style="width: 400px; height: 400px;"  loop autoplay></lottie-player>
     </div>
   </div>
 </div>
